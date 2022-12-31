0525771a2fac0aaa60de7b0fd8f7d2f0c766b547
Jessie <Jessiesu3@gmail.com>
2022-12-30 23:26:49 -0500

    Building website with react

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/HomePage.css';
+
+function HomePage(){
+    return (
+        <div className="HomepageContainer">
+            <button className='CommenceButton'>
+                    Commence Studying
+            </button>
+        </div>
+    )
+}
+
+export default HomePage;
